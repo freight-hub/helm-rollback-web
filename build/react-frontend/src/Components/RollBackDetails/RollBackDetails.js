bda1c12bc5c20f60c2f36002e506bd3e238936d1
Martyn <m@rtyn.berlin>
2021-03-05 15:28:23 +0100

    Frontend kinda done, Dockerfile updated

--- build/react-frontend/src/Components/RollBackDetails/RollBackDetails.js
+++ build/react-frontend/src/Components/RollBackDetails/RollBackDetails.js
@@ -1,21 +1,36 @@
-import { LazyLog, ScrollFollow  } from 'react-lazylog';
-import  React, {useEffect} from 'react';
-import { Container } from '@material-ui/core';
+import  React, {useEffect, Container} from 'react';
 
 function RollBackDetails() {
+    
     const namespace = window.location.href.split("/")[4]
     const releasename = window.location.href.split("/")[5]
     const revision = window.location.href.split("/")[6]
+    const sampleData = ["command not run, we're in frontend dev mode"]
+    const [commandState,setCommandState] = React.useState({loading:true,'commandData':[]});
     const fullURL = "/dorollback/"+namespace+"/"+releasename+"/"+revision
-    return (
-        
-         <ScrollFollow
-        startFollowing={true}
-        render={({ follow, onScroll }) => (
-            <LazyLog height={1000} url={fullURL} /> 
-        )}
-        />
-    )
+
+    useEffect(() => {
+        if (window.location.href.split("/")[2].substr(0,14) === "localhost:3000") {
+            setCommandState({commandData: sampleData, loading: false })
+        } else {
+          let actualURL = fullURL
+          fetch(actualURL).then((res) => res.json().then((data)=>{
+            setCommandState({commandData: data, loading: false })
+          }));
+        }
+      }, [setCommandState]);
+      
+      if (commandState.loading) {
+        return <p>Waiting for helm command to finish.... Seems streaming data into react is actually hard and noone bothers maintaining a nice component for this. :(</p>
+      } else {
+        return (
+            <div>
+            <tt>{commandState.commandData}</tt>
+            </div>
+        )
+      }
+    
+    //<p>Wait a bit and press back and refresh.  Once this isn't a hackday project it might be better.</p>
 }
 
 
