5ba5de3a4d279555f3e1950a65b19fe0e6b87cc1
Daniel Lamando <daniel.lamando@forto.com>
2021-03-15 18:31:46 +0100

    Deduplicate table related code into common files

---
+++ build/react-frontend/src/Components/EnhancedTableHead/EnhancedTableHead.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TableCell, TableHead, TableRow, TableSortLabel } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+export default function EnhancedTableHead(props) {
+  const { headCells, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+EnhancedTableHead.propTypes = {
+  headCells: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+};
--- build/react-frontend/src/Components/ReleaseDetails/ReleaseDetails.js
+++ build/react-frontend/src/Components/ReleaseDetails/ReleaseDetails.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Paper } from '@material-ui/core';
-import PropTypes from 'prop-types';
-import 'react-confirm-alert/src/react-confirm-alert.css';
+import { Button, Container, Typography, Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@material-ui/core';
+
+import EnhancedTableHead from '../EnhancedTableHead/EnhancedTableHead.jsx';
+import { stableSort, getComparator } from '../../util/sorting.js';
 
 const sampleData = [{"revision":4,"updated":"2020-07-01T08:02:46.685420988Z","status":"superseded","chart":"unleash-0.1.0","app_version":"b4563c9baa7617b137d8b272b7882968452ee952","description":"Upgrade complete"},{"revision":5,"updated":"2020-07-02T07:52:45.2940391Z","status":"superseded","chart":"unleash-0.1.0","app_version":"4ef59b8397f0f20e20a8955682829454a7031618","description":"Upgrade complete"},{"revision":6,"updated":"2020-07-02T11:04:21.905965626Z","status":"superseded","chart":"unleash-0.1.0","app_version":"87202bbfe0c323a26d7646261b8fdc60577fd1d4","description":"Upgrade complete"},{"revision":7,"updated":"2020-07-02T11:19:52.505235401Z","status":"superseded","chart":"unleash-0.1.0","app_version":"9ad5627b22f8a04708a3590a01cd1c0f5ee0fe60","description":"Upgrade complete"},{"revision":8,"updated":"2020-07-02T12:06:32.407068701Z","status":"superseded","chart":"unleash-0.1.0","app_version":"eadca9f31526d68a9d6fac17f32918d814f7398e","description":"Upgrade complete"},{"revision":9,"updated":"2020-07-03T10:11:12.806231454Z","status":"superseded","chart":"unleash-0.1.0","app_version":"cc75c65ffdca5dc085df96a4b15107df43a4bfa7","description":"Upgrade complete"},{"revision":10,"updated":"2020-10-07T12:48:20.868948428Z","status":"superseded","chart":"unleash-0.1.0","app_version":"1c6111dbef0c74e3d12c68e7d67f031b6e790dd1","description":"Upgrade complete"},{"revision":11,"updated":"2020-10-08T08:04:06.065204968Z","status":"superseded","chart":"unleash-0.1.0","app_version":"8ac30783abeab398a3ab2d82488983de9f919772","description":"Upgrade complete"},{"revision":12,"updated":"2020-10-09T13:49:15.64685762Z","status":"superseded","chart":"unleash-0.1.0","app_version":"6368e944160609e840b120666c0494dd3e1c8f3c","description":"Upgrade complete"},{"revision":13,"updated":"2021-01-28T08:48:47.189032409Z","status":"deployed","chart":"unleash-0.1.0","app_version":"67e9b20ce66cabc65f6d85a6ab0072ee90a966d8","description":"Upgrade complete"}]
 const sampleLoginData = {"email":"fake@forto.com"}
@@ -17,102 +18,22 @@ const headCells = [
   { id: 'action', numeric: true, disablePadding: false, label: 'Action' },
 ];
 
-
-function descendingComparator(a, b, orderBy) {
-  if (!isNaN(parseInt(a[orderBy])) && (!isNaN(parseInt(b[orderBy])))) {
-    if (parseInt(b[orderBy]) < parseInt(a[orderBy])) {
-      return -1;
-    }
-    if (parseInt(b[orderBy]) > parseInt(a[orderBy])) {
-      return 1;
-    }
-    return 0;
-  }
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function EnhancedTableHead(props) {
-  const { classes, order, orderBy, onRequestSort } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
 export default function ReleaseDetails() {
   const useStyles = makeStyles({
     table: {
       minWidth: 650,
     },
   });
-  const [appState, setAppState] = React.useState('userData');
+  const [appState, setAppState] = React.useState({});
   const [releasesState, setReleasesState] = React.useState({loading:true,'releaseData':[]});
-  const [order, setOrder] = React.useState('listOrder');
-  const [orderBy, setOrderBy] = React.useState('listOrderBy');
+  const [order, setOrder] = React.useState('desc');
+  const [orderBy, setOrderBy] = React.useState('revision');
   const classes = useStyles();
   const namespace = window.location.href.split("/")[4]
   const releasename = window.location.href.split("/")[5]
   const releasesURL = "/helm-history/"+namespace+"/"+releasename
 
-  const handleRequestSort = (event, property) => {
+  const handleRequestSort = (property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
@@ -162,17 +83,14 @@ export default function ReleaseDetails() {
       return <p>Sorry, still loading...</p>
     }
     return (
-      <TableContainer component={Paper}
-          order={order}
-          orderBy={orderBy}
-          onRequestSort={handleRequestSort}>
+      <TableContainer component={Paper}>
         <Table className={classes.table} size="small" aria-label="simple table">
-          <EnhancedTableHead onRequestSort={handleRequestSort}>
-            <TableRow>
-              <TableCell>Release Name</TableCell>
-              <TableCell align="right">Revision</TableCell>
-            </TableRow>
-          </EnhancedTableHead>
+          <EnhancedTableHead
+              headCells={headCells}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
           <TableBody>
           {stableSort(releases, getComparator(order, orderBy))
             .map(row => (
