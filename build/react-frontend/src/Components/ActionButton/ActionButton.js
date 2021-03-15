b586a0f0997ea42f3d2c2e8d46cfc2392cb9495d
Daniel Lamando <daniel.lamando@forto.com>
2021-03-15 22:25:50 +0100

    Remove unused react file

--- build/react-frontend/src/Components/ActionButton/ActionButton.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
-
-export default function ActionButton() {
-  return <Button variant="contained" color="primary" href="/login">⚙️</Button>
-}
