76b602eb37d2b760eba1e90d9539f9fbfb3dc957
Daniel Lamando <daniel.lamando@forto.com>
2024-08-20 12:01:51 +0200

    Redo the buildchain with vite

---
+++ build/react-frontend/eslint.config.js
@@ -0,0 +1,62 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['build'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
+
+
+// "import/order": [
+//   "warn",
+//   {
+//     "alphabetize": {
+//       "order": "asc",
+//       "caseInsensitive": true
+//     },
+//     "newlines-between": "always"
+//   }
+// ],
+// "key-spacing": [
+//   "warn",
+//   {
+//     "beforeColon": false,
+//     "afterColon": true
+//   }
+// ],
+// "object-curly-spacing": [
+//   "warn",
+//   "always"
+// ],
+// "array-bracket-spacing": [
+//   "warn",
+//   "always"
+// ],
+// "comma-spacing": [
+//   "warn",
+//   {
+//     "before": false,
+//     "after": true
+//   }
+// ]
